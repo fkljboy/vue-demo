4d4009de4b62719976fd23959ac99d414cd09dc9
xiaoqi shao <fengkoulangjianboy@gmail.com>
2020-07-03 10:17:09 +0800

    first commit

---
+++ vue_demo1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <button @click="jump">一键报障</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  el:"#app",
+  methods:{
+    jump(){
+      window.open('http://www.baidu.com')
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
